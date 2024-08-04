ce7cc63dc647e7e06ddf428bd2f87034c70edb73
Mwapsam <mwapsam@gmail.com>
2024-08-04 09:47:08 +0200

    Updates

--- scanpay/settings.py
+++ scanpay/settings.py
@@ -69,13 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "scanpay.wsgi.application"
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 default_database = {
     "ENGINE": os.environ.get("POSTGRES_ENGINE", "django.db.backends.sqlite3"),
     "NAME": os.environ.get(
